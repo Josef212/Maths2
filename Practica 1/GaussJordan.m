2bbe7de94471416b99e3ec114ff11a40a22ae004
Josef21296 <jcasanovasv@hotmail.com>
2016-04-24 20:43:13 +0200

    GaussJordan

--- Practica 1/GaussJordan.m
+++ Practica 1/GaussJordan.m
@@ -1,2 +1,34 @@
-function  b, A  = GausJordan(x, flag)
+function [x, flag]  = GaussJordan(b, A)
 
+vecSize = size(b);
+matrixSize = size(A);
+
+if matrixSize(1) ~= matrixSize(2) || matrixSize(1) ~= vecSize(2)
+    x = nan;
+    flag = -1;
+else
+    Aa = horzcat(A, b');
+    display('Matrix A concatenated with the vector b: ');
+    display(Aa);
+    AaMatrixSize = size(Aa);
+    Vp;
+    for columns = AaMatrixSize(2)
+        
+        
+        if pivot == 0
+            flag = 1;
+        else
+            flag = 3;
+        end
+        
+        for rows = AaMatrixSize(1)
+            
+        end
+        
+    end
+    
+    for rows = AaMatrixSize(1)
+       x(rows,:) = Aa(Vp(rows), end)/Aa(Vp(rows), rows);
+    end
+    
+end
