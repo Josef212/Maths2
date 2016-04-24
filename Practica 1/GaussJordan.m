bf3dc87af3c88b01d936e954c8f45a02fa68c93c
Josef21296 <jcasanovasv@hotmail.com>
2016-04-24 23:11:22 +0200

    Some changes

--- Practica 1/GaussJordan.m
+++ Practica 1/GaussJordan.m
@@ -7,12 +7,12 @@ if matrixSize(1) ~= matrixSize(2) || matrixSize(1) ~= vecSize(2)
     x = nan;
     flag = -1;
 else
-    Aa = horzcat(A, b');
+    Aa = [A; b'];
     display('Matrix A concatenated with the vector b: ');
     display(Aa);
     AaMatrixSize = size(Aa);
     Vp;
-    for columns = AaMatrixSize(2)
+    for columns = 1:AaMatrixSize(2)
         
         
         if pivot == 0
@@ -21,13 +21,13 @@ else
             flag = 3;
         end
         
-        for rows = AaMatrixSize(1)
+        for rows = 1:AaMatrixSize(1)
             
         end
         
     end
     
-    for rows = AaMatrixSize(1)
+    for rows = 1:AaMatrixSize(1)
        x(rows,:) = Aa(Vp(rows), end)/Aa(Vp(rows), rows);
     end
     
